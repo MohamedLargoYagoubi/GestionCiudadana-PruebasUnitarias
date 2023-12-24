d275fe22537e35c80f0c391010d21c42820c9492
MoreTheo <theomorenolomero@gmail.com>
2023-12-24 17:06:49 +0100

    do class SingleBiometricData

--- src/data/BiometricData.java
+++ src/data/BiometricData.java
@@ -1,4 +1,21 @@
 package data;
 
 public class BiometricData {
+
+    private final SingleBiometricData faceBiometricData;
+    private final SingleBiometricData fingerprintBiometricData;
+
+
+    public BiometricData(SingleBiometricData faceBiometricData, SingleBiometricData fingerprintBiometricData) {
+        this.faceBiometricData = faceBiometricData;
+        this.fingerprintBiometricData = fingerprintBiometricData;
+    }
+
+    public SingleBiometricData getFaceBiometricData() {
+        return faceBiometricData;
+    }
+
+    public SingleBiometricData getFingerprintBiometricData() {
+        return fingerprintBiometricData;
+    }
 }
--- src/data/SingleBiometricData.java
+++ src/data/SingleBiometricData.java
@@ -1,4 +1,37 @@
 package data;
 
+import java.util.Arrays;
+
 public class SingleBiometricData {
+
+    private final byte[] KeyData;
+
+    public SingleBiometricData(byte[] keyData) {
+        if(keyData == null) throw new NullPointerException("The Key data can't be null");
+        this.KeyData = keyData;
+    }
+
+    public byte[] getKeyData() {
+        return KeyData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SingleBiometricData that = (SingleBiometricData) o;
+        return Arrays.equals(KeyData, that.KeyData);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(KeyData);
+    }
+
+    @Override
+    public String toString() {
+        return "SingleBiometricData{" +
+                "KeyData=" + Arrays.toString(KeyData) +
+                '}';
+    }
 }
