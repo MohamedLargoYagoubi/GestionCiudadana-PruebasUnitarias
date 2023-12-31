544cc09fcbdd5e2e63594c6a5c5eb68f13a7487a
MoreTheo <theomorenolomero@gmail.com>
2023-12-31 19:26:29 +0100

    test of doubles of HumanBiometricScanerDouble

--- test/doublesTest/HumanBiometricScannerDoubleTest.java
+++ test/doublesTest/HumanBiometricScannerDoubleTest.java
@@ -1,2 +1,48 @@
-public class HumanBiometricScannerDoubleTest {
+import doubles.HumanBiometricScannerIncorrect;
+import doubles.HumanBiometricScannerIncorrectFace;
+import doubles.HumanBiometricScannerIncorrectFinger;
+import doubles.HumanBiometricScannerOk;
+import exceptions.HumanBiometricScanningException;
+import interfaces.HumanBiometricScannerInterfaceTest;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HumanBiometricScannerDoubleTest implements HumanBiometricScannerInterfaceTest {
+
+    HumanBiometricScannerOk hbsOk;
+    HumanBiometricScannerIncorrectFinger hbsIncorrectFinger;
+    HumanBiometricScannerIncorrectFace hbsIncorrectFace;
+    HumanBiometricScannerIncorrect hbsIncorrect;
+
+
+    @BeforeEach
+    public void setUp() {
+        hbsOk = new HumanBiometricScannerOk();
+        hbsIncorrectFinger = new HumanBiometricScannerIncorrectFinger();
+        hbsIncorrectFace = new HumanBiometricScannerIncorrectFace();
+        hbsIncorrect = new HumanBiometricScannerIncorrect();
+    }
+
+    @Override
+    public void scanBiometricOkTest() throws HumanBiometricScanningException {
+        assertEquals(hbsOk.scanFaceBiometrics().getKeyData().length,hbsOk.scanFingerprintBiometrics().getKeyData().length);
+        assertDoesNotThrow(() -> hbsOk);
+    }
+
+    @Override
+    public void incorrectScanFaceBiometricTest() throws HumanBiometricScanningException {
+        assertThrows(HumanBiometricScanningException.class, () -> hbsIncorrectFace.scanFaceBiometrics());
+        assertThrows(HumanBiometricScanningException.class, () -> hbsIncorrect.scanFaceBiometrics());
+        assertDoesNotThrow(() -> hbsOk.scanFaceBiometrics());
+        assertDoesNotThrow(() -> hbsIncorrectFace.scanFingerprintBiometrics());
+    }
+
+    @Override
+    public void incorrectScanFingerprintBiometricTest() throws HumanBiometricScanningException {
+        assertThrows(HumanBiometricScanningException.class, () -> hbsIncorrect.scanFingerprintBiometrics());
+        assertThrows(HumanBiometricScanningException.class, () -> hbsIncorrectFinger.scanFingerprintBiometrics());
+        assertDoesNotThrow(() -> hbsOk.scanFingerprintBiometrics());
+        assertDoesNotThrow(() -> hbsIncorrectFinger.scanFaceBiometrics());
+    }
 }
--- test/doublesTest/interfaces/HumanBiometricScannerInterfaceTest.java
+++ test/doublesTest/interfaces/HumanBiometricScannerInterfaceTest.java
@@ -1,4 +1,16 @@
 package interfaces;
 
+import exceptions.HumanBiometricScanningException;
+import org.junit.jupiter.api.Test;
+
 public interface HumanBiometricScannerInterfaceTest {
+
+    @Test
+    void scanBiometricOkTest() throws HumanBiometricScanningException;
+    @Test
+    void incorrectScanFaceBiometricTest() throws HumanBiometricScanningException;
+
+    @Test
+    void incorrectScanFingerprintBiometricTest() throws HumanBiometricScanningException;
+
 }
